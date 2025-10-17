ec87ffffea769cf5d8bdab206adf959b38e4fb37
Balu <balupasumarthi1@gmail.com>
2025-10-17 21:21:14 +0530

    fix: cors

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,42 +1,40 @@
-import Navbar from './navBar'
+import Navbar from "./navBar";
 import { Outlet, useNavigate } from "react-router-dom";
 import axios from "axios";
-import {BASE_URL} from "../utils/constants";
-import { useDispatch, useSelector } from 'react-redux';
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const Body = () => {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userData = useSelector((store) => store.user);
 
-  const fetchUser = async() =>{
-    if (userData) return;
-    try{
-      const res = await axios.get(BASE_URL+"/profile/view" ,{withCredentials:true});
-      dispatch(addUser(res.data));
-    }
-    catch(err){
-      if(err.status ===401){
-        navigate("/login");
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userData) return;
+      try {
+        const res = await axios.get(BASE_URL + "/profile/view", {
+          withCredentials: true,
+        });
+        dispatch(addUser(res.data));
+      } catch (err) {
+        if (err?.response?.status === 401) {
+          navigate("/login");
+        }
+        console.error(err);
       }
-      console.error(err);
-    }
-  };
-
-
-  useEffect(()=>{
-      fetchUser();
-  }, []);
+    };
+    fetchUser();
+  }, [userData, dispatch, navigate]);
 
   return (
     <div>
-      <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
